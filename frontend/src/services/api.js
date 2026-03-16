7996872284e0c1b7ded858be4ef703390da33aaa
rania-jb <rnya001@gmail.com>
2026-03-16 20:46:48 +0100

    car rental app

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://luxdrive-plateforme-de-location-de.onrender.com/api",
 });
 // Attach token to every request automatically
 API.interceptors.request.use((config) => {
